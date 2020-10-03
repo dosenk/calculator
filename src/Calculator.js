07a1afea510084fd4d5ab622ef9056f5f0fba3f8
Dmitry Osipenko <eympel@gmail.com>
2020-10-03 21:12:08 +0300

    feat: added method - decimal

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,10 +1,6 @@
 export default class Calculator {
     constructor() {
-        // this.memoryNumber = ''
-        // this.currentNumber = ''
         this.operationMemory = ''
-        // this.operationCurrent = ''
-        // this.prevCurrentNumber = ''
         this.operationCounter = 0
         this.memoryNewNumberFlag = false;
         this.displayInfo = document.querySelector('#monitor')
@@ -17,7 +13,6 @@ export default class Calculator {
             if (event.target.classList.contains('number')) {
                 let number = event.target.innerText
                 this.numberPress(number)
-                // console.log(event.target.innerText);
             }
         })
         // обработчик на математические операции
@@ -25,7 +20,6 @@ export default class Calculator {
             if (event.target.classList.contains('operator')) {
                 let operation = event.target.innerText
                 this.operationPress(operation)
-                // console.log('| ' + this.memoryNumber + ' - memoryNumber','| ' + this.currentNumber + ' - currentNumber','| ' + this.operationMemory + '  operationMemory','| ' + operation + ' operation');
             }
         })
         // обработчик на CE и С
@@ -33,13 +27,12 @@ export default class Calculator {
             if (event.target.classList.contains('clear-btn')) {
                 let id = event.target.innerText
                 this.clear(id)
-                console.log(event.target.innerText);
             }
         })
         // обработчик на .
         document.addEventListener('click', (event) => {
             if (event.target.classList.contains('decimal')) {
-                console.log(event.target.innerText);
+                this.decimal()
             }
         })
     }
@@ -47,35 +40,43 @@ export default class Calculator {
 
     // метод обработки нажатия на цифры
     numberPress(number) {
-        console.log(this.memoryNewNumberFlag);
-        if (this.operationCounter === 0)  {
-            this.displayInfo.value = ''
-        }
+        if (this.operationCounter === 0) this.displayInfo.value = ''
         if (this.memoryNewNumberFlag) {
-            this.displayMain.value = number;
+            if (this.displayMain.value === '-') this.displayMain.value += number
+            else this.displayMain.value = number;
             this.memoryNewNumberFlag = false;
         } else {
-            if (this.displayMain.value === '') {
-                this.displayMain.value = number;
-            } else {
-                this.displayMain.value += number;
-            }
+            if (this.displayMain.value === '' || this.displayMain.value === '0')  this.displayMain.value = number;
+            else this.displayMain.value += number;
         }
     }
 
 
     // метод обработки нажатия на математические операции
     operationPress(operation) {
+        if (operation === '-' && (this.displayMain.value === '' || this.displayMain.value === '-' || this.memoryNewNumberFlag === true)) {
+            if(this.operationMemory !== '=') {
+                this.displayMain.value = '-'
+                return 
+            }
+        }
 
-        this.memoryNumber = +this.displayMain.value
+        
 
-        if (operation === '√') {
+        if (operation === '√') { 
+            if (this.displayMain.value < 0) {
+                alert('Введены неверные данные')
+                return
+            } else if (this.displayMain.value === '-') {
+                return
+            }
             this.displayMain.value = Math.sqrt(this.displayMain.value)
-
         }
 
         if (!this.memoryNewNumberFlag || this.operationCounter === 0) {
-            if (operation === '√') return
+            this.memoryNumber = +this.displayMain.value
+
+            if (operation === '√' || this.displayMain.value === '-') return
             this.memoryNewNumberFlag = true
             this.prevCurrentNumber = this.currentNumber
             
@@ -86,25 +87,34 @@ export default class Calculator {
             } else if (this.operationMemory === '*') {
                 this.currentNumber *= this.memoryNumber;
             } else if (this.operationMemory === '/') {
+                if (this.memoryNumber === 0) {
+                    alert('Введены неверные данные. Делить на ноль не предусмотрено данным калькулятором')
+                    return
+                }
                 this.currentNumber /= this.memoryNumber;
             } else if (this.operationMemory === '^') {
+                if (this.currentNumber === 0 && this.memoryNumber < 0) {
+                    alert('Введены неверные данные')
+                    return
+                }
                 this.currentNumber = Math.pow(this.currentNumber, this.memoryNumber);
             } else {
                 this.currentNumber = this.memoryNumber;
             }
-
                 if (this.operationMemory === '=' && operation === '=') this.operationMemory = '' // запрет на вывод повторного =
                 if (this.operationMemory === '' && operation === '=') return                     // запрет на вывод повторного =
-
+                if (!Number.isInteger(this.currentNumber)) {
+                    if (operation === '=') this.currentNumber = +this.currentNumber.toFixed(10)
+                }
                 this.displayMain.value = this.currentNumber
                 this.drowDisplayInfo(operation, this.prevCurrentNumber)
-                
                 this.operationMemory = operation // меняем операцию
-        } else if (operation !== '=') {
-            console.log(2);
-            this.drowDisplayInfo(operation, this.prevCurrentNumber, true) // обрезаем 
-            this.operationMemory = operation
-        }
+        } 
+        // else if (operation !== '=') {
+        //     console.log(2);
+        //     this.drowDisplayInfo(operation, this.prevCurrentNumber, true) // обрезаем 
+        //     this.operationMemory = operation
+        // }
     }
 
     drowDisplayInfo(operation, prevCurrentNumber, flag = false) { // отображает инфу на втором инпуте
@@ -113,18 +123,14 @@ export default class Calculator {
             if (flag) {
                 this.displayInfo.value = this.displayInfo.value.substr(0, this.displayInfo.value.length - 2) + ' ' + operation
             } else {
-                console.log('asdasdasdasdads');
                 this.displayInfo.value += ' ' + this.memoryNumber + ' ' + operation
             }
-            this.operationCounter += 1
+            this.operationCounter ++
         } else {
             this.memoryNewNumberFlag = true
             if (prevCurrentNumber !== '') {
-                if (this.operationCounter > 1) {
-                    this.displayInfo.value = this.displayInfo.value + ' ' + this.memoryNumber + ' ='
-                } else {
-                    this.displayInfo.value = prevCurrentNumber + ' ' + this.operationMemory + ' ' + this.memoryNumber + ' =' 
-                }
+                if (this.operationCounter > 1) this.displayInfo.value = this.displayInfo.value + ' ' + this.memoryNumber + ' ='
+                else this.displayInfo.value = prevCurrentNumber + ' ' + this.operationMemory + ' ' + this.memoryNumber + ' =' 
                 this.operationCounter = 0 // обнуляем счетчик после равно
             } 
         }
@@ -132,8 +138,7 @@ export default class Calculator {
 
     clear(id) {
         this.displayMain.value = ''
-        this.memoryNewNumberFlag = true
-
+        this.memoryNewNumberFlag = false
         if (id === 'C' || this.operationMemory === '=') { // или если предыдущая операция была рано обнуляем все
             this.memoryNumber = ''
             this.currentNumber = ''
@@ -142,5 +147,18 @@ export default class Calculator {
             this.displayInfo.value = ''
             this.operationCounter = 0
         }
+    }
+
+    decimal() {
+        let localDecimalMemory = this.displayMain.value
+        if (this.memoryNewNumberFlag || localDecimalMemory === '' || localDecimalMemory === '-') {
+          localDecimalMemory = localDecimalMemory === '-' ? '-0.' : '0.'
+          this.memoryNewNumberFlag = false;
+        } else {
+          if (localDecimalMemory.indexOf('.') === -1) {
+            localDecimalMemory += '.';
+          }
+        }
+        this.displayMain.value = localDecimalMemory;
     }
 }
--- src/app.js
+++ src/app.js
@@ -4,5 +4,19 @@ window.onload = () => {
 
   let calculator = new Calculator()
   calculator.addListenersOnKeys()
+
+
+
+  let wraper = document.querySelector('.helper')
+  let result = document.querySelector('#helper_result')
+  let input = document.querySelectorAll('input')
+  wraper.addEventListener('click', (event) => {
+    let counter = 0
+    input.forEach((item) => {
+      if (item.checked) counter++
+    })
+    result.innerText = counter * 2
+  })
+
  
 }   
